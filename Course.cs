657fe2229be6c5745e40ce1a63edf7ab223961f7
kasperhhk@hotmail.com <kasperhhk@hotmail.com>
2025-01-12 20:10:48 +0100

    walls

--- Course.cs
+++ Course.cs
@@ -4,23 +4,22 @@ using System.Numerics;
 namespace NanoGolf;
 public class Course
 {
-    public Vector2[] Shape;
+    public Rectangle[] Walls;
     public Vector2 Start;
     public Vector2 Goal;
 
-    public Course(Vector2[] shape, Vector2 start, Vector2 goal)
+    public Course(Rectangle[] walls, Vector2 start, Vector2 goal)
     {
-        Shape = shape;
+        Walls = walls;
         Start = start;
         Goal = goal;
     }
 
     public void Draw()
     {
-        for (var i = 1; i < Shape.Length; i++)
+        foreach (var wall in Walls)
         {
-            Raylib.DrawLineEx(Shape[i - 1], Shape[i], 5, Color.Blue);
-            Raylib.DrawCircleV(Shape[i], 2.5f, Color.Blue);
+            Raylib.DrawRectangleRec(wall, Color.Blue);
         }
 
         Raylib.DrawCircleV(Start, 10, Color.Green);
@@ -28,12 +27,11 @@ public class Course
     }
 
     public static Course Default => new Course([
-        new Vector2(200, 200),
-        new Vector2(800, 200),
-        new Vector2(800, 500),
-        new Vector2(500, 500),
-        new Vector2(500, 800),
-        new Vector2(200, 800),
-        new Vector2(200, 200)
+        new Rectangle(200, 200, 600, 5),
+        new Rectangle(800, 200, 5, 300),
+        new Rectangle(500, 500, 305, 5),
+        new Rectangle(500, 500, 5, 300),
+        new Rectangle(200, 800, 305, 5),
+        new Rectangle(200, 200, 5, 600)
     ], new Vector2(350, 700), new Vector2(700, 350));
 }
